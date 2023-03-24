ed069862842bd20f34e8b45a75896601e8034111
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-03-24 14:53:27 -0400

    imported Character to index. Also imported useStaet to App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [details, useDetails] = useState("");
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -13,6 +13,6 @@ const App = () => {
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import "./App.css";
+import App from "./App";
+import Character from "./components/Character";
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <App />
+    <Character />
+  </>,
+
+  document.getElementById("root")
+);
