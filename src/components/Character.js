7fe7320d4b679c386b81069eca101119525c9367
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-03-26 01:36:50 -0400

    Completed the NVP Set the list of characters into state. Rendered the characters to the DOM. Built a React component named Character to render each individual character. Mapped over the list in state, and for each character rendered a Character to the page. The components are styled with styled components.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,25 @@
 // Write your Character component here
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
 
-const Character = () => {
+const StyledCharacter = styled.div`
+    width= 18rem;
+    display: flex;
+    justify-content: space-around;
+    padding: 8px;
+`;
+
+const StyledName = styled.h2`
+  border: 5px solid black;
+  border-radius: 6%;
+`;
+
+const Character = (props) => {
   return (
-    <div>
-      <h2>Hi Bloomtech!</h2>
-    </div>
+    <StyledCharacter>
+      <StyledName>{props.people.name}</StyledName>
+    </StyledCharacter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./App.css";
 import App from "./App";
-import Character from "./components/Character";
+import "./App.css";
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(
-  <>
-    <App />
-    <Character />
-  </>,
-
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
